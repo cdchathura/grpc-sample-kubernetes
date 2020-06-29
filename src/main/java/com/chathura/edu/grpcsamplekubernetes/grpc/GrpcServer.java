27b719ac524d2f8b1fb9780ea67873432dd10ad4
chathura Dissanayake <cdchathura@gmail.com>
2020-06-30 01:17:01 +0800

    Added port as a env variable

--- src/main/java/com/chathura/edu/grpcsamplekubernetes/grpc/GrpcServer.java
+++ src/main/java/com/chathura/edu/grpcsamplekubernetes/grpc/GrpcServer.java
@@ -3,6 +3,7 @@ package com.chathura.edu.grpcsamplekubernetes.grpc;
 import com.chathura.edu.grpcsamplekubernetes.service.HelloServiceImpl;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -11,11 +12,14 @@ import java.io.IOException;
 @Component
 public class GrpcServer {
 
+    @Value("${server.port}")
+    private Integer port;
+
     @PostConstruct
     public void initServer() throws IOException, InterruptedException {
-        Server server = ServerBuilder.forPort(8080)
+        Server server = ServerBuilder.forPort(port)
                 .addService(new HelloServiceImpl()).build();
-        System.out.println("Starting server...");
+        System.out.println("Starting server... on "+ port);
         server.start();
         System.out.println("Server started!");
         server.awaitTermination();
